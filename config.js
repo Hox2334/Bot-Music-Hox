2edcf45462a7ac3b44b23ece38697f4982fe16de
Hox2334 <rehim.loloopnulls.yahoo.fr@gmail.com>
2024-10-11 17:27:49 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,44 @@
+module.exports = {
+    app: {
+        token: "MTI5MTMzMDUzNzMxODkxMjAwMQ.GliE61.IQc1yopCT_oLHL2p3E_6wIFSV10XPjm7AuugG0",
+        stream: 'test uhq',
+        global: true,
+        guild: "1294251220034125876", 
+        extraMessages: false,
+        loopMessage: false,
+        lang: 'en',
+        enableEmojis: false,
+    },
+
+    emojis:{
+        'back': '⏪',
+        'skip': '⏩',
+        'ResumePause': '⏯️',
+        'savetrack': '💾',
+        'volumeUp': '🔊',
+        'volumeDown': '🔉',
+        'loop': '🔁',
+    },
+
+    opt: {
+        DJ: {
+            enabled: false,
+            roleName: '',
+            commands: []
+        },
+        Translate_Timeout: 10000,
+        maxVol: 100,
+        spotifyBridge: true,
+        volume: 75,
+        leaveOnEmpty: true,
+        leaveOnEmptyCooldown: 30000,
+        leaveOnEnd: true,
+        leaveOnEndCooldown: 30000,
+        discordPlayer: {
+            ytdlOptions: {
+                quality: 'highestaudio',
+                highWaterMark: 1 << 25
+            }
+        }
+    }
+};
